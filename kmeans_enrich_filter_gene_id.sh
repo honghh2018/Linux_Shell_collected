091a79763e72168bf6822f60ede9741e488f9504
Poplar <41668708+honghh2018@users.noreply.github.com>
2018-08-24 17:09:22 +0800

    Update kmeans_enrich_filter_gene_id.sh

--- kmeans_enrich_filter_gene_id.sh
+++ kmeans_enrich_filter_gene_id.sh
@@ -1,7 +1,7 @@
 #awk $2=11 second column equal "11",print line owned "11"
 #-d indicate seperate symbol character,default tab; -f1 indicate first column
-$cat kmeans_cluster.txt |awk '$2=11{print $0}' |cut -f1 -d'	' |less 
-$cat kmeans_cluster.txt |awk '$2=11{print $0}' |cut -f1 -d'	' > file  #use perl to build the script
+$cat kmeans_cluster.txt |awk '$2==11{print $0}' |cut -f1 -d'	' |less 
+$cat kmeans_cluster.txt |awk '$2==11{print $0}' |cut -f1 -d'	' > file  #use perl to build the script
 #comments:
 #Kmeans cluster,first classify,then cluster
 #heatmap cluster was arrangement cluster differ with Kmeans cluster 

d67a9645c83f3bf9cc0cf5bee23f0dee99ff83d0
Alsace <41668708+honghh2018@users.noreply.github.com>
2018-09-17 09:30:28 +0800

    Update print_header_and_column.sh

--- print_header_and_column.sh
+++ print_header_and_column.sh
@@ -1,6 +1,7 @@
 
 head -1 Data_Assess/AllSample_GC_Q.stat && less Data_Assess/AllSample_GC_Q.stat |grep L283 |awk '{print $2}'  #print header　and $2 column
-
+###awk　condition judgement 
+ head -1 Data_Assess/AllSample_GC_Q.stat && cat Data_Assess/AllSample_GC_Q.stat|awk '{if(/L283/) print $2}'
 
 
 
